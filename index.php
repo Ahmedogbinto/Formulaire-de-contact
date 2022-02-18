2160f0c27e2ee111444b656f170375496dc8ebb4
AhmedOg <39011405+Ahmedogbinto@users.noreply.github.com>
2022-02-18 14:52:23 -0600

    Validation des données du fomulaire

--- index.php
+++ index.php
@@ -1,16 +1,50 @@
 <?php 
 
-$firstname = $name = $email = $message = "";                           // 1ère fois j'aimerais que les champs soient vident. 
+$firstname = $name = $email = $message = "";                                         // 1ère fois j'aimerais que les champs soient vident. 
+$firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
+
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $firstname = VerifyInput($_POST["firstname"]);                                 // pour la deuxième fois j'aimerais sauvegarder les valeurs des champs.
-    $name = VerifyInput($_POST["name"]);
-    $email = VerifyInput($_POST["email"]);
-    $phone = VerifyInput($_POST["phone"]);
-    $message = VerifyInput($_POST["message"]);
+    $firstname = verifyInput($_POST["firstname"]);                                 // pour la deuxième fois j'aimerais sauvegarder les valeurs des champs.
+    $name = verifyInput($_POST["name"]);
+    $email = verifyInput($_POST["email"]);
+    $phone = verifyInput($_POST["phone"]);
+    $message = verifyInput($_POST["message"]);
+
+    if(empty($firstname))
+    {
+        $firstnameError = "Je veux connaitre ton prénom !";
+    }
+    if(empty($name))
+    {
+        $nameError = "Et oui je veux tout savoir, même ton nom !";
+    }
+    if(empty($message))
+    {
+        $messageError = "Qu'es ce tu veux me dire ?";
+    }
+    if(!isEmail($email))
+    {
+        $emailError = "Tu essaies de me roubler? c'est pas un email ça ! :)";
+    }
+    if(!isPhone($phone))
+    {
+        $phoneError = "Que des chiffres et des espaces stp...";
+    }
 }
 
-function VerifyInput($var)
+function isPhone($var)
+{
+    return preg_match("/^[0-9 ]+$/", $var);
+}
+
+
+function isEmail($var)
+{
+    return filter_var($var, FILTER_VALIDATE_EMAIL);
+}
+
+function verifyInput($var)
 {
     $var = trim($var);                                                           // Permet d eviter des injections de scripts, a partir de l URL pour la securite 
     $var = stripcslashes($var);                                                  // supprimer les caracteres anti slashes
@@ -52,31 +86,32 @@ function VerifyInput($var)
                         <div class="col-md-6">
                             <label for="firstname">Prénom<span class="blue"> *</span></label>
                             <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Votre prénom" value ="<?php echo $firstname; ?>">
-                            <p class="comments">Message d'erreur </p>
+                           
+                            <p class="comments"><?php echo $firstnameError; ?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="name">Nom<span class="blue"> *</span></label>
                             <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" value ="<?php echo $name; ?>">
-                            <p class="comments">Message d'erreur </p>
+                            <p class="comments"><?php echo $nameError; ?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="email">Email<span class="blue"> *</span></label>
-                            <input type="text" id="email" name="email" class="form-control" placeholder="Votre email" value ="<?php echo $email; ?>">
-                            <p class="comments">Message d'erreur </p>
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" value ="<?php echo $email; ?>">
+                            <p class="comments"><?php echo $emailError; ?></p>
                         </div>
 
                         <div class="col-md-6">
                             <label for="phone">Téléphone</label>
-                            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" value ="<?php echo $phone; ?>">
-                            <p class="comments">Message d'erreur </p>
+                            <input type="tel" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" value ="<?php echo $phone; ?>">
+                            <p class="comments"><?php echo $phoneError; ?></p>
                         </div>
 
                         <div class="col-md-12">
                             <label for="message">Message<span class="blue"> *</span></label>
                             <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4"><?php echo $message; ?></textarea>
-                            <p class="comments">Message d'erreur </p>
+                            <p class="comments"><?php echo $messageError; ?></p>
                         </div>
 
                         <div class="col-md-12">
@@ -89,7 +124,7 @@ function VerifyInput($var)
 
                     </div>
 
-                    <p class="thank-you">Votre message a bien été envoyé. Merci de m'avoir contacter :)</p>
+                    <p class ="thank-you"> Votre message a bien été envoyé. Merci de m'avoir contacter :) </p>
                 </form>
             </div> 
 
